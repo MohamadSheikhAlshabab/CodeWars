199b2b2152f367d7cd3c86e04b700e90742513da
Mohamad Sheikh Alshabab <60566868+MohamadSheikhAlshabab@users.noreply.github.com>
2022-11-18 23:41:31 +0300

    Create Friend_or_Foe?.js

---
+++ Friend_or_Foe?.js
@@ -0,0 +1,22 @@
+/*
+Description:
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+*/
+
+const friend = friends => friends.filter(friend => friend.length == 4);
+
+
+friend(["Ryan", "Kieran", "Mark"]); // ["Ryan", "Mark"])
+friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]); //["Ryan"])
+friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]); // ["Jimm", "Cari", "aret"])
+friend(["Love", "Your", "Face", "1"]); // ["Love", "Your", "Face"])
