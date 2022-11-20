2ea87bd5aafbcf424e17c4c0b0e20ab3ba1d841a
Mohamad Sheikh Alshabab <60566868+MohamadSheikhAlshabab@users.noreply.github.com>
2022-11-20 23:59:01 +0300

    Create V A P O R C O D E.js

---
+++ V A P O R C O D E.js
@@ -0,0 +1,31 @@
+/*
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+Note that spaces should be ignored in this case.
+Examples
+
+"Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+"Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+*/
+
+#Solution #1
+function vaporcode(string) {
+  return [...string.replace(/\s/g,"")].map(a=>`${a.toUpperCase()}`).join("  ");
+}
+#Solution #2
+function vaporcode(s) {
+  return s.toUpperCase().match(/[^ ]/g).join`  `
+}
+#Solution #3
+function vaporcode(string) {
+  return string.toUpperCase().replace(/ /g,'').split('').join('  ')
+}
+#Solution #4
+const vaporcode = string => string.toUpperCase().match(/\S/g).join(`  `);
+#Solution #5
+function vaporcode(string) {
+  return string.toUpperCase().split(" ").join("").split("").join("  ");
+}
+
+vaporcode("Lets go to the movies"); //"L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+vaporcode("Why isnt my code working"); //"W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G"
